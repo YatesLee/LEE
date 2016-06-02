f17a64727d6665219e7d15fea00a521059b3e6a0
bwfstu <bwfstu@liya.local>
2016-06-02 15:13:14 +0800

    Initial Commit

---
+++ ApplePai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePai
+//
+//  Created by bwfstu on 16/6/2.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
